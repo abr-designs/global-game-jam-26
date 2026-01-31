6a48ed74ea24f2e7df41d4d739fdc53d7e5c7398
Alex Bedard-Reid <alexbedardreid@gmail.com>
2026-01-31 14:15:16 -0800

    WIP Shooting Setup

---
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Projectiles
+{
+    public class Projectile : MonoBehaviour
+    {
+        private float speed;
+        private Vector3 currentDirection;
+        //private 
+    }
+}
