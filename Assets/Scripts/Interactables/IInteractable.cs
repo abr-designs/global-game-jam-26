22933f0d0b1b8ff84fa6f7e8e85be15320bef84f
Alex Bedard-Reid <alexbedardreid@gmail.com>
2026-01-31 11:37:05 -0800

    WIP Interactables

---
+++ Assets/Scripts/Interactables/IInteractable.cs
@@ -0,0 +1,7 @@
+﻿namespace Interactables
+{
+    public class IInteractable
+    {
+        private float InteractionDistance { get; }
+    }
+}
---
+++ Assets/Scripts/Interactables/InteractableManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Samples.CharacterController3D.Scripts;
+using UnityEngine;
+using Utilities.Debugging;
+
+namespace Interactables
+{
+    public class InteractableManager : MonoBehaviour
+    {
+        private static List<IInteractable> s_interactables;
+
+        [SerializeField]
+        private CharacterController3D characterController3D;
+
+        [SerializeField]
+        private float interactionRange;
+
+        [SerializeField]
+        private float dotThreshold;
+
+        [SerializeField, Min(1)]
+        private int checkFrequency;
+
+        //================================================================================================================//
+
+        public static void Register(IInteractable interactable)
+        {
+            s_interactables ??= new List<IInteractable>();
+            
+            s_interactables.Add(interactable);
+        }
+
+        public static void DeRegister(IInteractable interactable)
+        {
+            s_interactables.Remove(interactable);
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void CheckForInteractables()
+        {
+            
+        }
+
+        private void OnDrawGizmos()
+        {
+            var pos = characterController3D.transform.position;
+            
+            Draw.Circle(pos, Vector3.up, Color.white, interactionRange);
+        }
+    }
+}
