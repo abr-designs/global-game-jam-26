88a3c4677c3d76409b8fac42009124d44d7dbe34
Alex Bedard-Reid <alexbedardreid@gmail.com>
2026-01-30 23:59:12 -0800

    Camera Hiding Object Logic

---
+++ Assets/Scripts/Utility/CameraObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using Utilities.Debugging;
+
+namespace Utility
+{
+    public class CameraObject : MonoBehaviour
+    {
+        public Vector3 Direction => transform.TransformVector(hideWhenLocalDirection);
+        
+        [SerializeField]
+        private Renderer renderer;
+        [SerializeField]
+        private Vector3 hideWhenLocalDirection;
+
+        private bool m_isVisible = true;
+
+        private void OnEnable()
+        {
+            CameraObjectHider.Register(this);
+        }
+
+        private void OnDisable()
+        {
+            CameraObjectHider.DeRegister(this);
+        }
+
+        public void SetVisible(bool state)
+        {
+            //Don't try and set the same value
+            if (state == m_isVisible)
+                return;
+            
+            renderer.enabled = state;
+            m_isVisible = state;
+        }
+
+#if UNITY_EDITOR
+
+        private void OnDrawGizmosSelected()
+        {
+            Draw.Arrow(renderer.bounds.center, Direction, Color.blue);
+        }
+
+#endif
+    }
+}
---
+++ Assets/Scripts/Utility/CameraObjectHider.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraObjectHider : MonoBehaviour
+    {
+        private static List<CameraObject> s_cameraObjects;
+
+        [SerializeField, Min(1)]
+        private int checkFrequency = 1;
+
+        [SerializeField, Range(-1f, 1f)]
+        private float dotThreshold;
+
+        private int m_checkCounter;
+
+        public static void Register(CameraObject cameraObject)
+        {
+            s_cameraObjects ??= new List<CameraObject>();
+            
+            s_cameraObjects.Add(cameraObject);
+        }
+        public static void DeRegister(CameraObject cameraObject)
+        {
+            s_cameraObjects.Remove(cameraObject);
+        }
+
+        private void LateUpdate()
+        {
+            switch (checkFrequency)
+            {
+                case > 1 when m_checkCounter++ >= checkFrequency:
+                    m_checkCounter = 0;
+                    CheckAllObjects();
+                    break;
+                case 1:
+                    CheckAllObjects();
+                    break;
+            }
+        }
+
+        private void CheckAllObjects()
+        {
+            var camForward = transform.forward.normalized;
+            for (int i = s_cameraObjects.Count - 1; i >= 0; i--)
+            {
+                var otherDir = s_cameraObjects[i].Direction.normalized;
+                var dot = Vector3.Dot(camForward, otherDir);
+
+                
+                s_cameraObjects[i].SetVisible(dot > dotThreshold);
+            }
+        }
+    }
+}
